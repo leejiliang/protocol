2fe5a5abac0e3b0c1cc5f6d8d4513d1c3188c325
jiliang.li <leejiliang@126.com>
2024-10-17 19:35:29 +0800

    feat: modify the defaultQueueSize from 100 to 1000

--- webhook/url_notifier.go
+++ webhook/url_notifier.go
@@ -51,7 +51,7 @@ type HTTPClientParams struct {
 	ClientTimeout time.Duration
 }
 
-const defaultQueueSize = 100
+const defaultQueueSize = 1000
 
 // URLNotifier is a QueuedNotifier that sends a POST request to a Webhook URL.
 // It will retry on failure, and will drop events if notification fall too far behind
